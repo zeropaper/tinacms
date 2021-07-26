a4f696ac9ceb66f972ff2602975408b1e48055c4
Jeff See <jeffsee.55@gmail.com>
2021-07-26 08:21:08 -0700

    Make tinacms-inline, final-form and react-final-form direct deps on react-tinacms-editor

--- examples/tina-cloud-starter/pages/_app.js
+++ examples/tina-cloud-starter/pages/_app.js
@@ -14,13 +14,20 @@ limitations under the License.
 import dynamic from 'next/dynamic'
 import { TinaEditProvider } from 'tinacms/dist/edit-state'
 const TinaCMS = dynamic(() => import('tinacms'), { ssr: false })
+import { MarkdownFieldPlugin } from 'react-tinacms-editor'
 
 const App = ({ Component, pageProps }) => {
   return (
     <>
       <TinaEditProvider
         editMode={
-          <TinaCMS isLocalClient={true} {...pageProps}>
+          <TinaCMS
+            cmsCallback={(cms) => {
+              cms.plugins.add(MarkdownFieldPlugin)
+            }}
+            isLocalClient={true}
+            {...pageProps}
+          >
             {(livePageProps) => <Component {...livePageProps} />}
           </TinaCMS>
         }
